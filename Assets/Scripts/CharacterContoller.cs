250e2bd3fb4019952f9671260321b2ebd71548f5
Dylan <dylanryan431@gmail.com>
2024-09-23 09:54:22 -0300

    Added Camera Script

--- Assets/Scripts/CharacterContoller.cs
+++ Assets/Scripts/CharacterContoller.cs
@@ -14,10 +14,10 @@ public class CharacterContoller : MonoBehaviour
     }
 
 
-    private void Update()
+    private void FixedUpdate()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        characterController.Move(move * (Time.deltaTime * _moveSpeed));
+        characterController.Move(move * (Time.fixedDeltaTime * _moveSpeed));
     }
 }
