42200fcd382f1ce11a86fc0605f18809952fb17d
Dylan <dylanryan431@gmail.com>
2024-09-25 10:58:40 -0300

    Player and goblin are prefabs
    -Main menu basic setup

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager manager;
+    void Awake()
+    {
+        //If GameManager doesnt exist set this as the manager and dont destruction on load
+        if (manager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            manager = this;
+        }
+        //If GameManager already exists destroy the dupe
+        else if (manager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
